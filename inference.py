4832c54ef4aa8d62a83b5a76efc0a0c0a2ce1b1d
Ruian7P <jay1317214313@outlook.com>
2025-11-29 18:35:49 -0500

    initial commit

---
+++ inference.py
@@ -0,0 +1,36 @@
+import torch
+from PIL import Image
+from transformers import AutoModel
+from huggingface_hub import hf_hub_download
+from torchvision.transforms import functional as F
+
+def load_image(img_path):
+    img = Image.open(img_path).convert("RGB")
+    # Resize the image to have a fixed height of 64 pixels
+    img = img.resize((img.width * 64 // img.height, 64))
+    img = F.to_tensor(img)
+    img = F.normalize(img, [0.5], [0.5])
+    return img
+
+# 1. Load the model
+model_path = "./results/pretrain_2e-5_ech5"
+model = AutoModel.from_pretrained(model_path, trust_remote_code=True)
+model.cuda()  # Move to GPU if available
+
+# 2. Prepare your inputs
+style_text = 'THE JOLLY IS "U"'
+gen_text = 'Ruian7P'
+img_path = "./dataset/sample/sample.png"
+style_img = load_image(img_path)
+style_img = style_img.cuda()
+
+# 3. Generate an image
+generated_pil_image = model.generate(
+    style_text=style_text,
+    gen_text=gen_text,
+    style_img=style_img,
+    max_new_tokens=64
+)
+
+# 4. Save the result
+generated_pil_image.save(model_path + "/generated_image.png")
